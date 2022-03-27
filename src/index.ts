492379031890d5b4ebeaadd691db18bc82fd8720
yjkwon07 <dudwns9073@nate.com>
2022-03-28 02:03:35 +0900

    [component]: Tab

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './components/Button';
+export * from './components/Input';
 export * from './components/Overlay';
 export * from './components/Svg';
 export * from './components/Switch';
+export * from './components/Tab';
 
 export * from './theme';
