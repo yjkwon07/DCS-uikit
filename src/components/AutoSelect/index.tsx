56499c245b87a6830c84279b79f4ed53a0dce5f3
yjkwon07 <dudwns9073@nate.com>
2022-04-12 17:23:51 +0900

    [component]: index export

--- src/components/AutoSelect/index.tsx
+++ src/components/AutoSelect/index.tsx
@@ -1,2 +1,9 @@
 export { default as AutoSelect } from './AutoSelect';
-export type { SelectProps, SelectButtonProps, SelectOptionProps } from './@types/types';
+export type {
+  AutoSelectButtonProps,
+  AutoSelectInputGroupProps,
+  AutoSelectInputProps,
+  AutoSelectOptionProps,
+  AutoSelectOptionsProps,
+  AutoSelectProps,
+} from './@types/types';
