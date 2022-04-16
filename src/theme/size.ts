ab6cfbac44e2d6651083c37bb581d4857d5dbacc
yjkwon07 <dudwns9073@nate.com>
2022-04-16 11:40:31 +0900

    [component]: Text

---
+++ src/theme/size.ts
@@ -0,0 +1,18 @@
+import { theme } from 'twin.macro';
+
+const size = {
+  base: theme`fontSize.base`,
+  xs: theme`fontSize.xs`,
+  sm: theme`fontSize.sm`,
+  lg: theme`fontSize.lg`,
+  xl: theme`fontSize.xl`,
+  '2xl': theme`fontSize.2xl`,
+  '3xl': theme`fontSize.3xl`,
+  '4xl': theme`fontSize.4xl`,
+  '5xl': theme`fontSize.5xl`,
+  '6xl': theme`fontSize.6xl`,
+  '7xl': theme`fontSize.7xl`,
+  '8xl': theme`fontSize.8xl`,
+};
+
+export default size;
