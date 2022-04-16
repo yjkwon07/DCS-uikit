b116799f77b9cc493b5fabbc97fcc2973e01b339
yjkwon07 <dudwns9073@nate.com>
2022-04-16 16:53:56 +0900

    [component]: index exclude

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './components/Box';
 export * from './components/Button';
 export * from './components/Flex';
 export * from './components/Grid';
+export * from './components/Heading';
 export * from './components/Input';
 export * from './components/Menu';
 export * from './components/Overlay';
@@ -11,5 +12,7 @@ export * from './components/Select';
 export * from './components/Svg';
 export * from './components/Switch';
 export * from './components/Tab';
+export * from './components/Tag';
+export * from './components/Text';
 
 export * from './theme';
