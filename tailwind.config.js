76f8c3e8c14ad80b8b21777631332676e9ac3a68
yjkwon07 <dudwns9073@nate.com>
2022-06-21 01:31:02 +0900

    [config]: tailwind config 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -136,60 +136,15 @@ module.exports = {
           secondary: 'var(--text-colors-tab-secondary)',
         },
       },
-      keyframes: {
-        'slide-down': {
-          from: {
-            transform: 'translateY(-60px)',
-            opacity: 0,
-          },
-          to: {
-            transform: 'translateY(0px)',
-            opacity: 1,
-          },
-        },
-        'fade-in-up-aqua': {
-          from: {
-            opacity: 0,
-            transform: 'translateY(20px)',
-            color: 'aqua',
-          },
-          to: {
-            opacity: 1,
-            transform: 'translateY(0)',
-          },
-        },
-        'fade-in-left': {
-          from: {
-            opacity: 0,
-            transform: 'translateX(30px) scaleX(1.1)',
-          },
-          to: {
-            opacity: 1,
-            transform: 'translateX(0)',
-          },
-        },
-      },
-      animation: {
-        'slide-down': 'slide-down 0.5s both',
-        'fade-in-up-aqua': 'fade-in-up-aqua 0.5s both',
-        'fade-in-left': 'fade-in-left 1s cubic-bezier(0.19, 1, 0.22, 1) both',
-      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    plugin(({ addBase, addComponents, addUtilities, theme }) => {
+    plugin(({ addBase, addComponents, addUtilities }) => {
       addBase({});
-      addComponents({
-        '.example': {
-          backgroundColor: theme('colors.white'),
-          borderRadius: theme('borderRadius.lg'),
-          padding: theme('spacing.6'),
-          boxShadow: theme('boxShadow.xl'),
-        },
-      });
+      addComponents({});
       addUtilities({
         '.z-overlay': {
           zIndex: 19,
