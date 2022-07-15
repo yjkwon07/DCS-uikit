a91551abed94d3114d3f55c5cecb22d2e1e4dfe3
yjkwon07 <dudwns9073@nate.com>
2022-07-15 18:35:00 +0900

    [component]: Input  - LayoutProps, SpaceProps  - PolymorphicComponentProps 적용 안함  - shouldForwardProp 적용 안함 => html tag write

--- src/utils/colorStyle.ts
+++ src/utils/colorStyle.ts
@@ -5,8 +5,16 @@ import { colors, textColor } from '../theme';
 
 const colorStyle = ({ background, color }: StyledColorProps) => {
   return css`
-    background: ${(background && get(colors, background)) || background};
-    color: ${(color && get(textColor, color)) || color};
+    background: ${
+      background && get(colors, background.replace(' !important', ''))
+        ? get(colors, background.replace(' !important', '')) + background.includes('!important') && ' !important'
+        : background
+    }};
+    color: ${
+      color && get(textColor, color.replace(' !important', ''))
+        ? get(colors, color.replace(' !important', '')) + color.includes('!important') && ' !important'
+        : color
+    }};
   `;
 };
 
