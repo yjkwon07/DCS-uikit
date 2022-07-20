778c72ec2b1b45d2ceae30f4d512cb976a3353de
yjkwon07 <dudwns9073@nate.com>
2022-07-20 09:24:30 +0900

    [storybook]: react is defined error - babel config 복구 - babel 설정 파일을 바라보지 않고 main에 직접 작성 - opt. webpackFinal loader 제거 - opt. typescript 제거

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,90 @@
+const path = require('path');
+const fs = require('fs');
+
+const babel = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+        importSource: '@emotion/react',
+      },
+      // @see https://github.com/storybookjs/storybook/issues/12881
+      'preset-react-jsx-transform', // Can name this anything, just an arbitrary alias to avoid duplicate presets
+    ],
+    '@emotion/babel-preset-css-prop',
+  ],
+  plugins: ['babel-plugin-twin', 'babel-plugin-macros'],
+};
+
 module.exports = {
   stories: ['../src/components/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-links'],
   core: {
     builder: 'webpack5',
   },
-  typescript: { reactDocgen: false },
+  babel: async (options) => {
+    const pluginsPackageNameList = babelOptionFlatName(babel.plugins);
+    const presetsPackageNameList = babelOptionFlatName(babel.presets);
+
+    options.plugins = options.plugins
+      .filter((plugin) => isDuplicatePackage(plugin, pluginsPackageNameList))
+      .concat(toPackageAbsolute(babel.plugins));
+
+    options.presets = options.presets
+      .filter((preset) => isDuplicatePackage(preset, presetsPackageNameList))
+      .concat(toPackageAbsolute(babel.presets));
+    return options;
+  },
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
     });
 
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
 };
+
+function packageAbsoluteDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+    }
+    currDir = dir;
+  }
+}
+
+function babelOptionFlatName(optionPackageList) {
+  return optionPackageList.map((optionPackage) => {
+    if (Array.isArray(optionPackage)) {
+      return packageAbsoluteDir(optionPackage[0]);
+    }
+    return packageAbsoluteDir(optionPackage);
+  });
+}
+
+function isDuplicatePackage(option, currentPackageList) {
+  if (Array.isArray(option)) {
+    return (
+      typeof option[0] === 'string' && !currentPackageList.find((currentPackage) => option[0].includes(currentPackage))
+    );
+  }
+  return !currentPackageList.find((currentPackage) => option.includes(currentPackage));
+}
+
+function toPackageAbsolute(packageList) {
+  return packageList.map((package) => {
+    if (Array.isArray(package) && typeof package[0] === 'string') {
+      return [packageAbsoluteDir(package[0]), ...package.slice(1)];
+    }
+    return packageAbsoluteDir(package);
+  });
+}
